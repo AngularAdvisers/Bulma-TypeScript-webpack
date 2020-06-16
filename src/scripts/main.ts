da1b3c08059509e2cd74b4e461444104839a4bf9
Angular Advisors <AngularAdvisors@gmail.com>
2020-06-16 10:05:25 -0600

    Put in script tag

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -21,30 +21,3 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
-
-// function to set a given theme/color-scheme
- function setTheme(themeName: string) {
-  localStorage.setItem('theme', themeName);
-  document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-export function toggleTheme() {
-  if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-light');
-  } else {
-      setTheme('theme-dark');
-  }
-}
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-  const slider = document.getElementById('slider') as HTMLInputElement;
-  if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-dark');
-      slider.checked = false;
-  } else {
-    setTheme('theme-light');
-    slider.checked = true;
- }
-})();
